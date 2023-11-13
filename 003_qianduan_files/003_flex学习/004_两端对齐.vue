a7ee7894143a36e603e31245338e7187deb57936
wwsit <1031839762@qq.com>
2023-11-13 21:33:21 +0800

    两端对齐

---
+++ 003_qianduan_files/003_flex学习/004_两端对齐.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="level0">
+
+        <div class="level1">
+            <div>元素1</div>
+            <div>元素2</div>
+        </div>
+
+        <div class="level2">
+            <div>元素3</div>
+        </div>
+    </div>
+</template>
+  
+<script>
+
+</script>
+  
+<style lang="less">
+
+.level0 {
+    display: flex;
+
+    justify-content: space-between;  // 让元素在两端对齐
+
+    .level1 { 
+        display: flex;
+    }
+
+    .level2 {
+        display: flex;
+    }
+}
+</style>
+  
