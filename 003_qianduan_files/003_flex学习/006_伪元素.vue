a73eef163d4a0988a696d667c51e18b2c780aa64
wwsit <1031839762@qq.com>
2023-11-19 22:24:33 +0800

    伪元素-扩大按钮点击范围

---
+++ 003_qianduan_files/003_flex学习/006_伪元素.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="level0">
+
+        <di>
+            <span class="button1">
+                <input type="checkbox">
+            </span>
+            <span>点击1</span>
+        </di>
+
+        <div>
+            <!-- 在按钮的父级元素上 进行伪元素设置 -->
+            <span class="button2">
+                <input type="checkbox">
+            </span>
+            <span>点击2</span>
+
+        </div>
+
+    </div>
+</template>
+  
+<script>
+
+</script>
+  
+<style lang="less">
+.level0 {
+
+    margin-top: 5%;
+    margin-left: 5%;
+
+
+    .button2 {
+        position: relative;
+    }
+
+    .button2 ::before {
+        content: "";
+        position: absolute;
+
+        // 可以重新设置点击区域的范围
+        top: 0px;
+        right: -200px;
+        bottom: 0px;
+        left: 0px;
+    }
+}
+</style>
