ef11a958be336ef779a5c0074253df5d5a120e74
wwsit <1031839762@qq.com>
2024-04-08 22:41:29 +0800

    增加文件

---
+++ 004_element-ui/005_upload文件上传.vue
@@ -0,0 +1,133 @@
+<template>
+  <el-upload ref="upload" class="upload-demo" action="" :limit="1" :on-exceed="handleExceed" :auto-upload=false
+    :on-change="handleSuccess" :on-remove="handleRemove" :file-list="fileList" :on-preview="handleFile">
+
+    <template #trigger>
+      <el-button type="primary">上传excel文件</el-button>
+    </template>
+    <template #tip>
+      <div class="el-upload__tip text-red">
+        请上传.png格式的图片
+      </div>
+    </template>
+  </el-upload>
+
+  <el-button type="primary" @click="getData()" class="click_button">加载附件</el-button>
+</template>
+
+<script>
+import { genFileId } from 'element-plus'
+import axios from 'axios'
+export default {
+
+
+  data() {
+    return {
+      fileList: [],
+      upload: "",
+      add_error_flag: false,
+      add_fast_use_appendix: false,
+    }
+  },
+
+  methods: {
+
+
+    // 只能上传一个文件
+    handleExceed(files) {
+      console.log(files)
+      this.$refs.upload.clearFiles()
+      const file = files[0]
+      file.uid = genFileId()
+      this.$refs.upload.handleStart(file)
+    },
+
+
+    open2(msg) {
+      this.$message({
+        message: msg,
+        type: 'error',
+      })
+    },
+
+    // 文件上传成功
+    handleSuccess(file, fileList) {
+      let filename = file.raw.name
+      if (filename.indexOf(".png") != -1) {
+        this.upload = file.raw
+
+        let formData = new FormData()
+        formData.append('files', this.upload)
+        formData.append("oaid", "test")
+
+        axios({
+          url: "http://192.168.255.10:5000" + "/api/saveData",
+          method: "post",
+          data: formData,
+        }).then((res) => {
+          if (res.data.result == "success") {
+            this.add_fast_use_appendix = "false"
+          }
+          else {
+            console.log(res)
+          }
+
+        }).catch((error) => {
+          console.log(error)
+        })
+
+
+
+      } else {
+        this.open2("请上传.png格式的图片")
+        this.$refs.upload.clearFiles()
+      }
+
+
+    },
+
+
+    // 移除文件
+    handleRemove(file, fileList) {
+      this.upload = ""
+      this.add_download_url = ""
+    },
+
+
+    // 点击上传列表中的文件
+    handleFile(files) {
+      window.open(this.add_download_url, '_blank');
+    },
+
+
+    // 加载附件的格式
+    getData() {
+      this.fileList = [{ name: "test.png", url: "https://d1.lanrentuku.com/upload/img/sucaiku/202010130648/5f84dd29906d3.png" }]
+    },
+
+
+  },
+
+
+  mounted() {
+
+  },
+
+
+}
+</script>
+
+<style lang="less">
+.upload-demo {
+
+  margin-top: 5%;
+  margin-left: 50%;
+  width: 500px;
+}
+
+
+.click_button {
+  margin-left: 50%;
+  margin-top: 5%;
+}
+</style>
---
+++ 004_element-ui/006_单选多选组件.vue
@@ -0,0 +1,107 @@
+<template>
+    <div class="area_all">
+      <div class="area1">
+        <div>单选-筛选选项</div>
+        <div>
+  
+          <el-select v-model="value" filterable placeholder="Select" style="width: 240px">
+            <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+          </el-select>
+        </div>
+      </div>
+  
+      <div class="area2">
+        <div>多选</div>
+        <div>
+          <el-select v-model="value2" filterable placeholder="Select" style="width: 240px" multiple clearable  collapse-tags
+        collapse-tags-tooltip>
+            <el-option v-for="item in options2" :key="item.value" :label="item.label" :value="item.value"  />
+          </el-select>
+        </div>
+      </div>
+    </div>
+  
+    <!-- https://element-plus.gitee.io/zh-CN/component/select.html -->
+  </template>
+  
+  <script>
+  import axios from 'axios'
+  export default {
+  
+  
+    data() {
+      return {
+        value: "",
+        value2:"",
+        options: [
+          {
+            value: 'Option1',
+            label: '选项名1',
+          },
+          {
+            value: 'Option2',
+            label: '选项名2',
+          },
+          {
+            value: 'Option3',
+            label: '选项名3',
+          },
+          {
+            value: 'Option4',
+            label: '选项名4',
+          },
+          {
+            value: 'Option5',
+            label: '选项名5',
+          },
+        ],
+        options2: [
+          {
+            value: 'Option1',
+            label: '选项名1',
+          },
+          {
+            value: 'Option2',
+            label: '选项名2',
+          },
+          {
+            value: 'Option3',
+            label: '选项名3',
+          },
+          {
+            value: 'Option4',
+            label: '选项名4',
+          },
+          {
+            value: 'Option5',
+            label: '选项名5',
+          },
+        ]
+      }
+    },
+  
+    methods: {
+  
+  
+    },
+  
+  
+    mounted() {
+  
+    },
+  
+  
+  }
+  </script>
+  
+  <style lang="less">
+  .area_all {
+  
+    margin-top: 5%;
+    margin-left: 45%;
+  
+    .area2{
+      margin-top: 5%;
+    }
+  }
+  </style>
